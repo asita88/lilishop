92c347c3e63cb99195ed4b1e37419a016aef049d
yanhai.weng <yanhai.weng@lkcoffee.com>
2024-12-11 00:45:53 +0800

    ff
    ff

--- docker-image.sh
+++ docker-image.sh
@@ -1,1 +1,26 @@
-mvn clean package docker:build  -DskipTests  -DpushImage
+cd /opt/server-api/
+cd admin
+docker build --rm -f ./Dockerfile -t admin:latest .
+
+cd buyer-api
+docker build --rm -f ./Dockerfile -t buyer-api:latest .
+
+cd common-api
+docker build --rm -f ./Dockerfile -t common-api:latest .
+
+cd consumer
+docker build --rm -f ./Dockerfile -t consumer:latest .
+
+cd im-api
+docker build --rm -f ./Dockerfile -t im-api:latest .
+
+cd manager-api
+docker build --rm -f ./Dockerfile -t manager-api:latest .
+
+cd seller-api
+docker build --rm -f ./Dockerfile -t seller-api:latest .
+
+cd xxl-job
+docker build --rm -f ./Dockerfile -t xxl-job:latest .
+
+#docker-compose -f docker-compose.yml restart
